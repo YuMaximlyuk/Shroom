ba926bf0e940b90e52c7744c042e6047f597b357
YuMaximlyuk <yumaximlyuk@gmail.com>
2020-05-05 04:33:18 +0700

    Fix UI and fonts.

---
+++ Assets/Scripts/UI/ButtonAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAudio : MonoBehaviour, IPointerEnterHandler
+{
+    private AudioSource m_AudioSource;
+    [SerializeField]
+    private AudioClip m_OnButtonAudio;
+    [SerializeField]
+    private AudioClip m_OnClickButtonAudio;
+
+    private void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnClickAudio()
+    {
+        m_AudioSource.clip = m_OnClickButtonAudio;
+        m_AudioSource.Play();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_AudioSource.clip = m_OnButtonAudio;
+        m_AudioSource.Play();
+    }
+}
